31426a0869e80956e2ad65426b58baf4519d8e76
Megi <102622566+megistankoja@users.noreply.github.com>
2022-04-23 18:48:18 +0200

    Add files via upload

---
+++ Liber.java
@@ -0,0 +1,19 @@
+interface Liber {
+  static final String bibloteka = "Bibloteka Shkoder";
+
+  void vendosTitullin(String titulli);
+
+  void vendosAutorin(String autori);
+
+  void vendosNumrinEFaqeve(int faqet);
+
+  void vendosKategorne(String Kategoria);
+
+  String merrTitullin();
+
+  String merrAutorin();
+
+  int merrNumrinEFaqeve();
+
+  String merrKategorine();
+}
